083356a8eff36957509d2594ab6d9ea36cd7dadb
tara97 <taraprasad9090@gmail.com>
2023-01-15 20:44:43 +0530

    Adding Each mealItem Ui

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css"
+import MealItem from "./MealItem/MealItem.js";
 const DUMMY_MEALS = [
     {
       id: 'm1',
@@ -27,9 +29,9 @@ const DUMMY_MEALS = [
   ];
 
   const AvailableMeals = () => {
-    const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+    const mealList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} price={meal.price} description={meal.description} />)
     return <section className={classes.meals}>
-        <ul>{mealList}</ul>
+        <Card><ul>{mealList}</ul></Card>
     </section>
   }
 
